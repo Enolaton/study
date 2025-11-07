fc1f7bff0b0da0c785ba2a7fdd934974184509d4
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-11-07 10:09:29 +0900

    [level 0] Title: 약수 구하기, Time: 0.93 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,6 +1,7 @@
 def solution(n):
     answer = []
+    # 1부터 n까지
     for i in range(1,n+1):
-        if n%i == 0:
-            answer.append(i)
-    return answer
+        if n%i == 0: # 입력값 n을 반복값i로 나누었을 때 나머지가 없으면, i는 n의 약수
+            answer.append(i) # 리스트에 i 추가
+    return answer # 리스트 반환
