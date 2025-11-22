18714ff342572f56541297fdfd9375defce2d752
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-11-22 13:14:23 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.09 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -21,12 +21,15 @@ def solution(a, b):
         for j in range(1,b+1):
             if b%j  == 0:
                 cnt_list.append(j)
+    # 분모의 약수 리스트 중 2와 5의 배수만 모을 리스트  
     tmp_list = [1]
+    # 약수 리스트 안에 2와 5를 제외한 약수가 포함된 경우 -> 무한소수 
     for k in range(len(cnt_list)):
         if cnt_list[k] % 2 == 0 or cnt_list[k] % 5 == 0:
             tmp_list.append(cnt_list[k])
+    # 분모의 약수 리스트 중에서 2와 5의 배수만 모은 것과 모든 약수 리스트 두개가 다르다면 무한소수
     if tmp_list != cnt_list:
         answer=2
-    else:
+    else: # 같은 경우 분모가 2와 5로만 이루어진 경우 -> 유한소수
         answer=1
     return answer
