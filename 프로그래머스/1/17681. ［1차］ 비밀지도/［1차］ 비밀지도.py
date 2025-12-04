83343a4618c6e269795c408ed6c88440ff6b9031
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-04 22:55:28 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.06 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,25 @@
+def solution(n, arr1, arr2):
+    answer = []
+    arr1_binary = []
+    arr2_binary = []
+    
+    # 10진수 배열을 2진수 리스트로 변환
+    for num in range(n):
+        arr1_binary.append(bin(arr1[num])[2:])
+        arr2_binary.append(bin(arr2[num])[2:])
+        
+    for a1,a2 in zip(arr1_binary,arr2_binary):
+        # 원소 X의 길이가 n보다 작을 경우, '100'인 경우 '00100'반환 
+        if len(a1) < n: 
+                arr1_binary[arr1_binary.index(a1)] = '0'*(n-len(a1))+a1
+        if len(a2) < n:
+                arr2_binary[arr2_binary.index(a2)] = '0'*(n-len(a2))+a2
+                
+    # 기본으로 '#'을 n만큼 두고, 두 배열의 원소 중 0이 있으면 (공백)을 변환
+    for row in range(n):
+        result = '#'*n
+        for column in range(n):
+            if arr1_binary[row][column] == '0' and arr2_binary[row][column] == '0':
+                result = result[:column]+' '+result[column+1:] # 슬라이싱을 이용해서 바뀐 문자열 변환
+        answer.append(result)
+    return answer
