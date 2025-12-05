34942774bcb752b674b9975fb29c4645e45828a7
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-05 10:14:34 +0900

    [level 2] Title: 올바른 괄호, Time: 8.15 ms, Memory: 9.78 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,11 +2,19 @@ def solution(s):
     answer = True
     check = list(s)
     check_num = 0
+    
+    # 왼쪽 괄호의 수와 오른쪽 괄호의 수의 개수를 비교
     if s.count('(') == s.count(')'):
+        # 여는 괄호로 끝날 경우 false
         if s[-1] == '(':
             answer = False
     else:
+        # 여는 괄호와 닫는 괄호의 개수가 다를 경우
          answer = False 
+            
+    # 여는 괄호와 닫는 괄호의 수가 같아도 위치에 따라서 다를 수 있음
+    # -1, 0, +1 을 사용해서 위치가 제대로 인지 판별함
+    # 디지털신호 인코딩, 디코딩에서 아이디어 받음
     for c in s:
         if c == '(':
             check_num += 1
