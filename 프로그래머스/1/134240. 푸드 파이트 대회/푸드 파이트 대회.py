19f1fc40093e67655017aa904d0597d58244a1b6
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-05 09:06:49 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.32 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,6 +1,8 @@
 def solution(food):
     food_list = []
-
+    # 선수 a가 왼쪽, 선수 b가 오른쪽에서 시작한다고 할때
+    # 왼쪽만 다 계산하고, 0을 추가한 후 왼쪽 계산을 역으로 출력한 후, 전체 반환
+    
     for i in range(1,len(food)):
         food_num = food[i]
         food_list.append(str(i)*(food_num//2))
