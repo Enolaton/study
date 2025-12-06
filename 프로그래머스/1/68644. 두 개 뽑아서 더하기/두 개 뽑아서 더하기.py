997a92cddf6ba896d7a97d29e33a6cce1ab4a62f
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-06 09:37:57 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 5.28 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,13 +1,18 @@
-from itertools import combinations as c
+"""
+numbers의 첫 번째 원소에 접근
+            ↓
+numbers의 두 번째 원소부터 마지막까지 접근하며 첫번째 원소와 더하기
+(단, 더한 값이 출력리스트 answer에 없어야 함)
+            ↓
+answer를 sort함수를 사용하여 오름차순 정렬
+"""
 
+# itertools-combinations 를 사용하지 않고 푸는 방법
 def solution(numbers):
-    # 조합을 이용해서 두개의 수를 뽑음
     answer = []
-    number_list = []
-    for a,b in c(numbers,2):
-        number_list.append(a+b)
-    for i in number_list:
-        if i not in answer:
-            answer.append(i)
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i]+numbers[j] not in answer:
+                answer.append(numbers[i]+numbers[j])
     answer.sort()
     return answer
