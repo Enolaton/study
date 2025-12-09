47cba53d1cfd68fb0561d6ca361c29ebd1f485d0
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-09 17:30:38 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -2,6 +2,7 @@ def solution(strings, n):
     answer = []
     c_list = [] # 인덱스 문자를 담을 리스트
     s_list = [] # 인덱스 문자로 비교한 순서를 strings와 매칭하여 담을 리스트
+    
     for s in strings:
         c_list.append(s[n])
     for s_out in zip(c_list, strings):
