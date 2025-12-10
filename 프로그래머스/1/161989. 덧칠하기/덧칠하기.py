66057ad4f55ed275b6a1b0fdc98153b907f94cbb
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-11 01:31:08 +0900

    [level 1] Title: 덧칠하기, Time: 19.94 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -8,12 +8,16 @@ def solution(n, m, section):
         section_list[s-1] = False
     
     for i in section:
+        # 인덱스한 값이 맨 마지막 빈 페이트 영역인 경우 
         if i+m > section[-1]:
             answer+=1
             break
+        # 인덱스한 값이 이미 페인트 칠된 경우
         if section_list[i-1] == True:
             continue
+        # 페인트 칠 실행
         for j in range(i-1,i+m-1):
             section_list[j] = True
+        # 횟수 +1
         answer+=1
     return answer
