9817fdb2b4039b99de3a5bbf6d0d0d38eb07114b
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-11 17:32:25 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,7 +1,10 @@
 def solution(s):
     str_list = s.split(' ')
     num_list = []
+    
     for c in str_list:
         num_list.append(int(c))
+        
     num_list = sorted(num_list)
+    
     return str(num_list[0]) + ' ' + str(num_list[-1])
