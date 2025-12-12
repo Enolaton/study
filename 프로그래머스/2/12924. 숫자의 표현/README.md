49cbad8fe3a6bbc9136e6f2b7e3088545f79ab3a
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-12 10:08:31 +0900

    [level 2] Title: 숫자의 표현, Time: 27.34 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
 
 ### 성능 요약
 
-메모리: 9.2 MB, 시간: 0.75 ms
+메모리: 9.25 MB, 시간: 27.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 09일 17:45:09
+2025년 12월 12일 10:08:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,11 +1,22 @@
 def solution(n):
     answer = 0
-    num_list = []
+    # 1부터 n까지 리스트로 생성 
+    num_list = [x for x in range(1,n+1)]
     
-    for i in range(1, n+1):
-        if n%i == 0:
-            num_list.append(i)
-    for num in num_list:
-        if num%2 != 0:
-            answer+=1
+    # i는 얼마나 많은 수를 짝지을 것인가에 대한 변수
+    # 예를 들어, i가 1이면 [1], [2], [3] 이고
+    # i가 2이면 [1,2], [2,3], [3,4]
+    for i in range(1,n+1):
+        # j는 이동에 관한 변수
+        # j가 1이면 [1], 2이면 [2]
+        for j in range(1,n+1):
+            tmp = num_list[i-1:i+j-1]
+            # 효율성 테스트 통과를 위해,
+            # 리스트의 합이 n을 초과하는 경우 중단
+            if sum(tmp) > n:
+                break
+            # 리스트의 합이 n과 동일한 경우 +1
+            if ((tmp[0]+tmp[-1])*(tmp[-1]-tmp[0]+1))/2 == n:
+                answer += 1
+                break
     return answer
