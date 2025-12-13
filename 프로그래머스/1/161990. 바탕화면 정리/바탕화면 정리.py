c5bc9b00dea85835501c9e45c4518016e8067a42
Leo_ <57707627+Enolaton@users.noreply.github.com>
2025-12-13 14:50:46 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.36 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,12 @@
+def solution(wallpaper):
+    answer = []
+    row = []
+    col = []
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                row.append(i)
+                col.append(j)
+                continue
+    answer = [min(row),min(col),max(row)+1,max(col)+1]
+    return answer
